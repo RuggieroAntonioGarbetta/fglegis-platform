a2884c39760808cdd16e003ed11d0151249afc97
RuggieroAntonioGarbetta <legal.fgconsulting@gmail.com>
2025-06-04 12:04:55 +0000

    Rimosso SMTP hardcoded, ora con variabili ambiente

---
+++ fglegis_platform/settings.py
@@ -0,0 +1,7 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'noreply@fglegis.com'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'FG LEGIS <noreply@fglegis.com>'
